fe38557581dc3284d25f7fac3dcf3a89b97a0ef1
Hanyu <8245snake@gmail.com>
2020-07-05 17:40:34 +0900

    天気予報に雨雲マップを追加

--- weather.go
+++ weather.go
@@ -28,41 +28,46 @@ func GetRainMap() string {
 }
 
 //GetWeatherReportFrame 今日と明日の天気のフレーム
-func GetWeatherReportFrame() {
+func GetWeatherReportFrame() ResponseWeather {
+	var data ResponseWeather
+	data.RainMapURL = GetRainMap()
+
 	URL := "https://weather.yahoo.co.jp/weather/jp/13/4410.html"
 
 	doc, err := goquery.NewDocument(URL)
 	if err != nil {
-		return
+		return data
 	}
 
 	html, err := doc.Find("div.forecastCity").Html()
 	if err != nil {
-		return
+		return data
 	}
 
 	//新規作成
 	file, err := os.OpenFile("./static/etc/iframe_weather.html", os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		return
+		return data
 	}
 	defer file.Close()
 	writer := bufio.NewWriter(file)
 	t := template.Must(template.ParseFiles("template/_iframe_weather.html"))
-	if err := t.Execute(writer, html); err != nil {
+	type ReportData struct {
+		FrameHTML string
+		ImageURL  string
+	}
+	values := ReportData{FrameHTML: html, ImageURL: data.RainMapURL}
+	if err := t.Execute(writer, values); err != nil {
 		log.Fatal(err)
 	}
 	//書き込み
 	writer.Flush()
+	return data
 }
 
 //天気予報
 func serveWeatherReport(w http.ResponseWriter, r *http.Request) {
-	var data ResponseWeather
-
-	data.RainMapURL = GetRainMap()
-	GetWeatherReportFrame()
-
+	data := GetWeatherReportFrame()
 	data.Message = "OK"
 	jsondata, _ := json.Marshal(data)
 	w.Write(jsondata)
